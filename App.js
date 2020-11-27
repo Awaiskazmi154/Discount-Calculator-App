815275c1dea9fa70fadb44e03c0ded4cdb43d049
Awaiskazmi154 <awaiskfiverr@gmail.com>
2020-11-27 19:45:23 +0500

    Input Fields are set

--- App.js
+++ App.js
@@ -8,8 +8,29 @@ import AssetExample from './components/AssetExample';
 
 export default function App() {
 
+  const [getOriginalPrice, setOriginalPrice] = useState('');
+  const [getDiscountPercent, setDiscountPercent] = useState('');
+
   return (
     <View style={styles.container}>
+      <Text>Enter Original Price</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType={'number-pad'}
+        placeholder={'e.g. 500'}
+        onChangeText={(val) => {
+          val >= 0 ? setOriginalPrice(val) : '';
+        }}
+      />
+      <Text>Enter Discount % </Text>
+      <TextInput
+        style={styles.input}
+        keyboardType={'number-pad'}
+        placeholder={'e.g. 20'}
+        onChangeText={(val) => {
+          val >= 0 && val <= 100 ? setDiscountPercent(val) : '';
+        }}
+      />
     </View>
   );
 }
